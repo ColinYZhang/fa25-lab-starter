f114cf2db3d9ceaf60751a630907f190ef56fed3
Colin Z <colinzhang@berkeley.edu>
2025-09-10 01:39:11 -0700

    one more fix

--- lab02/pwd_checker.c
+++ lab02/pwd_checker.c
@@ -33,7 +33,7 @@ bool check_range(char letter, char lower, char upper) {
 
 /* Returns true if PASSWORD contains at least one upper case letter, false otherwise */
 bool check_upper(const char *password) {
-    while (*password != '\1') {
+    while (*password != '\0') {
         bool is_in_range = check_range(*password, 'A', 'Z');
         if (is_in_range) {
             return true;
@@ -57,7 +57,7 @@ bool check_lower(const char *password) {
 
 /* Returns true if PASSWORD contains at least one number, false otherwise */
 bool check_number(const char *password) {
-    while (*password != '\1') {
+    while (*password != '\0') {
         if (check_range(*password, '0', '9')) {
             return true;
         }
